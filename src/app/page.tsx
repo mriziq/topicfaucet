60fdcfae547665d02353dc52cb7462cc24da5584
mriziq <amerkmriziq@gmail.com>
2023-09-21 00:52:55 -0700

    hide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,16 @@ export default function Home() {
         <Link href={"/about"}>
       <Image src={"./logo.svg"} alt='Topic Faucit Logo' width={128} height={180}/>
       </Link>
+      <Box 
+      as="div" 
+      display="flex" 
+      justifyContent="center" 
+      alignItems="center" 
+      animation={`${softlyFlash} 2s infinite`}
+      fontSize="lg"
+    >
+      Press<span style={{"marginLeft": 7, "marginRight": 7}}><Kbd>space</Kbd></span>for more
+    </Box>
       <Box>
       {domLoaded && (<FetchAPIComponent />)}
       </Box>
