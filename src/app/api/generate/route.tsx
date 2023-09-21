8425d51db3b502019d13ebf668ced4907bd6b02e
mriziq <amerkmriziq@gmail.com>
2023-09-20 20:02:00 -0700

    fix process env

--- src/app/api/generate/route.tsx
+++ src/app/api/generate/route.tsx
@@ -24,7 +24,7 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY ?? ""}`,
       },
       method: "POST",
       body: JSON.stringify(payload),
