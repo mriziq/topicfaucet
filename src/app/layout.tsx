7948e164217ab8361ddb4d35736b99b49be30e80
mriziq <amerkmriziq@gmail.com>
2023-09-21 00:28:48 -0700

    layout debug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <>
       <body>
         <Providers>
         {children}
         </Providers>
         </body>
-    </html>
+    </>
   )
 }
